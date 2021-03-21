275967d2543f636d7ee02bea77233552f5ff24d7
Michael Neuweiler <michael@NCC-1701D.local>
2021-03-21 20:36:51 +0100

    add new config elements for battery make Class self-instanciating

--- Config.cpp
+++ Config.cpp
@@ -8,27 +8,6 @@
 #include "Config.h"
 
 const char *Config::CONFIG_FILE = "/config.json";
-uint16_t Config::initialSolarPower;
-uint16_t Config::inverterUpdateInterval;
-uint16_t Config::pvOutPowerTolerance;
-int16_t Config::maxBatteryDischargeCurrent;
-int16_t Config::minBusVoltage;
-float Config::minPvVoltage;
-float Config::maxPvVoltage;
-uint16_t Config::powerAdjustment;
-uint16_t Config::minSolarPower;
-uint16_t Config::maxSolarPower;
-uint32_t Config::cutoffRetryTime;
-uint8_t Config::cutoffRetryMinBatterySoc;
-String Config::stationSsid;
-String Config::stationPassword;
-uint16_t Config::stationUpdateInterval;
-uint16_t Config::stationReconnectInterval;
-bool Config::outputAsCurrent;
-String Config::stationRequestPrefix;
-String Config::stationRequestPostfix;
-String Config::serverSsid;
-String Config::serverPassword;
 
 void Config::init()
 {
@@ -40,13 +19,13 @@ void Config::init()
 void Config::load() {
     File file = SPIFFS.open(CONFIG_FILE, "r");
     if (!file) {
-        Logger::error("Failed to open %s", CONFIG_FILE);
+        logger.error("Failed to open %s", CONFIG_FILE);
     }
 
     StaticJsonDocument < 1024 > doc;
     DeserializationError error = deserializeJson(doc, file);
     if (error)
-        Logger::error("Failed to parse file, using default configuration");
+        logger.error("Failed to parse file, using default configuration");
 
     inverterUpdateInterval = doc["inverter"]["interval"] | 300;
     initialSolarPower = doc["inverter"]["pv"]["power"]["initial"] | 1000;
@@ -60,6 +39,18 @@ void Config::load() {
     minBusVoltage = doc["inverter"]["bus"]["voltage"]["min"] | 390;
     cutoffRetryTime = doc["inverter"]["cutoffRetry"]["time"] | 300;
     cutoffRetryMinBatterySoc = doc["inverter"]["cutoffRetry"]["minBatterySoc"] | 50;
+
+
+    batteryCapacity = doc["battery"]["capacity"] | 100;
+    batteryType = doc["battery"]["type"] | BatteryType::LiIon;
+    batteryVoltageFullCharge = doc["battery"]["voltage"]["full"] | 28.4f;
+    batteryVoltageNominal = doc["battery"]["voltage"]["nominal"] | 25.6f;
+    batteryVoltageEmpty = doc["battery"]["voltage"]["empty"] | 21.6f;
+    batteryVoltageFloat = doc["battery"]["voltage"]["float"] | 24.5f;
+    batterySocCalculateInternally = doc["battery"]["soc"]["calculateInternally"] | true;
+    batteryRestDuration = doc["battery"]["soc"]["restDuration"] | 5;
+    batteryRestCurrent = doc["battery"]["soc"]["restCurrent"] | 10;
+    batterySocTriggerFloatOverride = doc["battery"]["soc"]["triggerFloatOverride"] | 0;
 
     stationSsid = String(doc["station"]["ssid"] | "myStation");
     stationPassword = String(doc["station"]["password"] | "stationPasswd");
@@ -77,27 +68,40 @@ void Config::load() {
 
 void Config::print()
 {
-    Logger::console("inverterUpdateInterval : %d", inverterUpdateInterval);
-    Logger::console("initialSolarPower: %d", initialSolarPower);
-    Logger::console("pvOutPowerTolerance: %d", pvOutPowerTolerance);
-    Logger::console("minSolarPower: %d", minSolarPower);
-    Logger::console("maxSolarPower: %d", maxSolarPower);
-    Logger::console("powerAdjustment: %d", powerAdjustment);
-    Logger::console("minPvVoltage: %f", minPvVoltage);
-    Logger::console("maxPvVoltage: %f", maxPvVoltage);
-    Logger::console("maxBatteryDischargeCurrent: %d", maxBatteryDischargeCurrent);
-    Logger::console("minBusVoltage: %d", minBusVoltage);
-    Logger::console("cutoffRetryTime: %d", cutoffRetryTime);
-    Logger::console("cutoffRetryMinBatterySoc: %d", cutoffRetryMinBatterySoc);
+    logger.console("inverterUpdateInterval : %dms", inverterUpdateInterval);
+    logger.console("initialSolarPower: %dW", initialSolarPower);
+    logger.console("pvOutPowerTolerance: %dW", pvOutPowerTolerance);
+    logger.console("minSolarPower: %dW", minSolarPower);
+    logger.console("maxSolarPower: %dW", maxSolarPower);
+    logger.console("powerAdjustment: %dW", powerAdjustment);
+    logger.console("minPvVoltage: %fV", minPvVoltage);
+    logger.console("maxPvVoltage: %fV", maxPvVoltage);
+    logger.console("maxBatteryDischargeCurrent: %dA", maxBatteryDischargeCurrent);
+    logger.console("minBusVoltage: %dV", minBusVoltage);
+    logger.console("cutoffRetryTime: %dsec", cutoffRetryTime);
+    logger.console("cutoffRetryMinBatterySoc: %d%%", cutoffRetryMinBatterySoc);
 
-    Logger::console("stationSsid: %s", stationSsid.c_str());
-    Logger::console("stationPassword: %s", stationPassword.c_str());
-    Logger::console("stationUpdateInterval: %d", stationUpdateInterval);
-    Logger::console("stationReconnectInterval: %d", stationReconnectInterval);
-    Logger::console("outputAsCurrent: %d", outputAsCurrent);
-    Logger::console("stationRequestPrefix: %s", stationRequestPrefix.c_str());
-    Logger::console("stationRequestPostfix: %s", stationRequestPostfix.c_str());
+    logger.console("batteryCapacity: %dAh", batteryCapacity);
+    logger.console("batteryType: %d", batteryType);
+    logger.console("batteryVoltageFullCharge: %fV", batteryVoltageFullCharge);
+    logger.console("batteryVoltageNominal: %fV", batteryVoltageNominal);
+    logger.console("batteryVoltageEmpty: %fV", batteryVoltageEmpty);
+    logger.console("batteryVoltageFloat: %fV", batteryVoltageFloat);
+    logger.console("batterySocTriggerFloatOverride: %f%%", batterySocTriggerFloatOverride);
+    logger.console("batteryRestDuration: %dsec", batteryRestDuration);
+    logger.console("batteryRestCurrent: %dA", batteryRestCurrent);
 
-    Logger::console("serverSsid: %s", serverSsid.c_str());
-    Logger::console("serverPassword: %s", serverPassword.c_str());
+    logger.console("stationSsid: %s", stationSsid.c_str());
+    logger.console("stationPassword: %s", stationPassword.c_str());
+    logger.console("stationUpdateInterval: %d", stationUpdateInterval);
+    logger.console("stationReconnectInterval: %d", stationReconnectInterval);
+    logger.console("outputAsCurrent: %d", outputAsCurrent);
+    logger.console("stationRequestPrefix: %s", stationRequestPrefix.c_str());
+    logger.console("stationRequestPostfix: %s", stationRequestPostfix.c_str());
+
+    logger.console("serverSsid: %s", serverSsid.c_str());
+    logger.console("serverPassword: %s", serverPassword.c_str());
 }
+
+Config config;
+
--- Config.h
+++ Config.h
@@ -16,40 +16,62 @@
 class Config
 {
 public:
-    static void init();
-    static void load();
+    enum BatteryType
+    {
+        LeadAcid = 0,
+        NiMh = 1,
+        LiIon = 2,
+        Other = 99
+   };
+
+    void init();
+    void load();
 
     static const char *CONFIG_FILE;
 
     // Inverter
-    static uint16_t initialSolarPower; // initial Power to start consumer with (in W)
-    static uint16_t inverterUpdateInterval; // at which interval the next data is requested from inverter (in ms)
-    static uint16_t pvOutPowerTolerance; // tolerance of higher out power against PV input (in W)
-    static int16_t maxBatteryDischargeCurrent; // allowed discharge current before throttling down consumer power (in A)
-    static int16_t minBusVoltage; // minimum bus voltage allowed before throttling down consumer power (in V)
-    static float minPvVoltage; // minimum PV voltage allowed before throttling down consumer power (in V)
-    static float maxPvVoltage; // PV voltage at which the consumer power can be increased (in V)
-    static uint16_t powerAdjustment; // amount of power increased/decreased when adjusting consumer power (in W)
-    static uint16_t minSolarPower; // minimum solar power to provide to the consumer (in W)
-    static uint16_t maxSolarPower; // maximum solar power to provide to the consumer (in W)
-    static uint32_t cutoffRetryTime; // time until a retry is started after a power cutoff due to minSolarPower (in sec)
-    static uint8_t cutoffRetryMinBatterySoc; // minimum battery soc to try a restart after power cutoff (in %)
+    uint16_t initialSolarPower; // initial Power to start consumer with (in W)
+    uint16_t inverterUpdateInterval; // at which interval the next data is requested from inverter (in ms)
+    uint16_t pvOutPowerTolerance; // tolerance of higher out power against PV input (in W)
+    int16_t maxBatteryDischargeCurrent; // allowed discharge current before throttling down consumer power (in A)
+    int16_t minBusVoltage; // minimum bus voltage allowed before throttling down consumer power (in V)
+    float minPvVoltage; // minimum PV voltage allowed before throttling down consumer power (in V)
+    float maxPvVoltage; // PV voltage at which the consumer power can be increased (in V)
+    uint16_t powerAdjustment; // amount of power increased/decreased when adjusting consumer power (in W)
+    uint16_t minSolarPower; // minimum solar power to provide to the consumer (in W)
+    uint16_t maxSolarPower; // maximum solar power to provide to the consumer (in W)
+    uint32_t cutoffRetryTime; // time until a retry is started after a power cutoff due to minSolarPower (in sec)
+    uint8_t cutoffRetryMinBatterySoc; // minimum battery soc to try a restart after power cutoff (in %)
+
+    // Battery
+    uint16_t batteryCapacity; // the capacity of the battery (in Ah)
+    BatteryType batteryType; // the type of battery used
+    float batteryVoltageFullCharge; // the voltage at which the battery pack is fully charged and charge should stop (in V)
+    float batteryVoltageNominal; // the nominal (resting) voltage of the fully charged battery pack (in V)
+    float batteryVoltageEmpty; // the battery voltage at which a resting battery is to be considered fully discharged (in V)
+    float batteryVoltageFloat; // the default float voltage to set to avoid trickle charging Li-Ion batteries (in V)
+    bool batterySocCalculateInternally; // if true we'll display the SOC / Ah by counting ourselfes, if fals we'll use the inverter's SOC (true/false)
+    uint8_t batterySocTriggerFloatOverride; // state of charge at which a float voltage charge will be triggered (in %, 0 to disable)
+    uint8_t batteryRestDuration; // if voltage < batteryVoltageEmpty this is the duration where load has to be below restCurrent before we declare the battery empty (in sec)
+    uint8_t batteryRestCurrent; // max current where we still consider the battery to be at rest with no signifikant load (in A)
 
     // Station
-    static String stationSsid;
-    static String stationPassword;
-    static uint16_t stationUpdateInterval; // at which interval the next update is sent to the consumer (in ms)
-    static uint16_t stationReconnectInterval; // at which interval the next connection attempt to the station is made (in ms)
-    static bool outputAsCurrent; // if true the consumer output is sent in Amps, otherwise in Watts
-    static String stationRequestPrefix; // the prefix for the HTTP request to the station
-    static String stationRequestPostfix; // the postfix for the HTTP request to the station
+    String stationSsid;
+    String stationPassword;
+    uint16_t stationUpdateInterval; // at which interval the next update is sent to the consumer (in ms)
+    uint16_t stationReconnectInterval; // at which interval the next connection attempt to the station is made (in ms)
+    bool outputAsCurrent; // if true the consumer output is sent in Amps, otherwise in Watts
+    String stationRequestPrefix; // the prefix for the HTTP request to the station
+    String stationRequestPostfix; // the postfix for the HTTP request to the station
 
     // Webserver
-    static String serverSsid;
-    static String serverPassword;
+    String serverSsid;
+    String serverPassword;
 
 private:
-    static void print();
+    void print();
 };
+
+extern Config config;
 
 #endif /* CONFIG_H_ */
